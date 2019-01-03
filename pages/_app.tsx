3c57267cda8fd4a432e74e4243ac4a4f1f184912
Yidong Li <liyidong@bytedance.com>
2019-01-03 17:42:18 +0800

    build(react-modelx): Upgrade to v1.1.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import App, { Container, NextAppContext } from 'next/app'
-import { Provider, registerModel } from 'react-modelx'
+import { Provider, Model } from 'react-modelx'
 
 import Layout from '../components/layout'
 
@@ -16,7 +16,7 @@ const models = {
   Todo
 }
 
-export const { useStore } = registerModel(models)
+export const { useStore } = Model(models)
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }: NextAppContext) {
